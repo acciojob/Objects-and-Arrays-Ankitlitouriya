6b3e2176bd35092bc362e5f5108599c19fc22222
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-07-23 14:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
+let cap1 = person;
 
 export {players,person,team,team1,cap1}
